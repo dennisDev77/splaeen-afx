a371221a15b4fffd778932a6ae442a288601c855
dennisDev77 <diniscamela5@gmail.com>
2025-02-28 10:11:04 +0100

    Update to Logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -5,10 +5,10 @@ import styles from '../../app/page.module.css'
 const Logo = () => {
   return (
 
-      <section className='flex justify-start items-center gap-2'>
+      <main className='flex justify-start items-center gap-2'>
         <Image src="/images/logo.png" alt="Carregando a logo S" className={styles.imgLogo} width={100} height={100}/>
         <h2 className='font-semibold'>Spleen AFX</h2>
-      </section>
+      </main>
   )
 }
 
